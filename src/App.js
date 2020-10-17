c8eb2a3a43e03e21736ad18f517a03288e743628
chokrihamza <chokrihamza1994@gmail.com>
2020-10-17 23:58:02 +0200

    check point state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+       fullName:"Chokri hamza",
+       bio:`Years | 2015-2018
+       Engineering degree– The National Higher Engineering School of
+       Tunis
+       Years | 2013-2015
+       Preparatory cycle– Preparatory Institute Of Engineering Studies
+       Of Gabes
+       Year | 2012-2013
+       Bachelor.`,
+       imgSRC:"./images/avatar.jpg",
+       profession:"Industrial Engineer",
+       show:true,
+       date:new Date()
+    };
+
+
+  }
+
+ toggleClick=()=>{
+   this.setState((state)=>({show:!state.show}));
+ }
+  Meniteur(){
+    this.setState({date:new Date()});
+  }
+
+  
+  componentDidMount() {
+    this.updateTime=setInterval(
+      ()=>this.Meniteur(),1000
+    );
+  }
+ componentWillUnmount() {
+   clearInterval(this.updateTime)
+  }
+
+  render() {
+     if(this.state.show){
+       return ( 
+         <div className="Person__description">
+         <h1 className="Timer">Hello the time is {this.state.date.toLocaleTimeString()}</h1>
+        <h1>{this.state.fullName}</h1>
+        <h1>{this.state.bio}</h1>
+        <img className="Avatar" src={this.state.imgSRC}  alt="Avatar chokri hamza " />
+         <h1>{this.state.profession}</h1>
+        
+        <button className="buttonToggle" onClick={this.toggleClick}>Toggle with your profile</button>
+       </div>);
+     }
+     return <div className="buttonFrame">
+     <h1 className="Timer">Hello the time is {this.state.date.toLocaleTimeString()}</h1>
+     <button className="buttonToggle" onClick={this.toggleClick}>Toggle with your profile</button>
+     </div>;
+     
+    
+  }
 }
 
-export default App;
+
+
+export default App
